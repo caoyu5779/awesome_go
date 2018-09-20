f411b3da6d741e695965e81660246dfdea483b02
caoyu5779 <caoyu5779@126.com>
2018-09-20 19:05:29 +0800

    弹幕练手

--- Barrage/server.go
+++ Barrage/server.go
@@ -1,10 +1,46 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"github.com/gorilla/websocket"
+)
+
+var (
+	upgrader = websocket.Upgrader{
+		//握手过程中 涉及到跨域问题，允许跨域
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+)
 
 func wsHandler(w http.ResponseWriter, r *http.Request) {
-	//
-	w.Write([]byte("hello"))
+	var (
+		conn *websocket.Conn
+		err error
+		data []byte
+	)
+	//完成协议转换
+	// Upgrade : webSocket
+	if conn, err = upgrader.Upgrade(w, r, nil) ; err != nil {
+		return
+	}
+
+	//webSocket.Conn
+	for {
+		// text | binary
+		if _, data, err = conn.ReadMessage(); err != nil {
+			goto ERR
+		}
+
+		if err = conn.WriteMessage(websocket.TextMessage, data); err != nil{
+			goto ERR
+		}
+	}
+
+	ERR:
+		conn.Close()
+
 }
 
 func main()  {
